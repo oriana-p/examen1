e4eb045e8e811acf24f7307359efbd077d0201fd
Oriana Piliado <55095888+oriana-p@users.noreply.github.com>
2020-02-28 10:56:12 -0600

    Add files via upload

---
+++ examen1-Oriana/scripts.js
@@ -0,0 +1,33 @@
+function mostrarFoto5() {
+
+    document.getElementById("foto").style.display = "block";
+}
+
+
+function ocultarFoto5() {
+
+    document.getElementById("foto").style.display = "none";
+}
+
+function validarTelefono5() {
+    /*
+    var validarTelefono5 = document.getElementById("telefono").value;
+    var msg = "";
+    var flagNumTelefono = false;
+    var flagNumCorrecto = false;
+    var flagCaracteresEspeciales = false;
+
+    var n = telefono.length;
+
+    if (n < 10) {
+        flagNumTelefono = true;
+        msg += "  El teléfono debe tener exactamente 10 números<br>";
+    }
+
+    if (n > 10) {
+        flagNumCorrecto = true;
+        msg += " El teléfono debe tener exactamente 10 números<br>";
+    }
+*/
+
+}
